45269f851e09814f749e9191c3442aaac5500208
amiyagiri007 <amiyakrushnagiri430@gmail.com>
2025-02-07 22:40:36 +0530

    minor changes

--- src/main/java/com/nt/commons/ActorConfiProperties.java
+++ src/main/java/com/nt/commons/ActorConfiProperties.java
@@ -13,4 +13,14 @@ import lombok.Data;
 public class ActorConfiProperties {
 
 	private Map<String, String> messages;
+
+	public Map<String, String> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(Map<String, String> messages) {
+		this.messages = messages;
+	}
+	
+	
 }
--- src/main/java/com/nt/model/Actor.java
+++ src/main/java/com/nt/model/Actor.java
@@ -18,5 +18,49 @@ public class Actor {
 	@Column(length = 20)
 	private String category;
 	private Long mobno;
+	public Actor(Integer actorId, String actorname, String category, Long mobno) {
+		super();
+		this.actorId = actorId;
+		this.actorname = actorname;
+		this.category = category;
+		this.mobno = mobno;
+	}
+	
+	public Actor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getActorId() {
+		return actorId;
+	}
+
+	public void setActorId(Integer actorId) {
+		this.actorId = actorId;
+	}
+
+	public String getActorname() {
+		return actorname;
+	}
+
+	public void setActorname(String actorname) {
+		this.actorname = actorname;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Long getMobno() {
+		return mobno;
+	}
+
+	public void setMobno(Long mobno) {
+		this.mobno = mobno;
+	}
+	
 
 }
