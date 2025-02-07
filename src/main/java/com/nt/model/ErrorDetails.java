dcd257edcf1c109f9222f011a297fa6e7de2d8d1
amiyagiri007 <amiyakrushnagiri430@gmail.com>
2025-02-07 22:52:58 +0530

    changes

--- src/main/java/com/nt/model/ErrorDetails.java
+++ src/main/java/com/nt/model/ErrorDetails.java
@@ -2,13 +2,8 @@ package com.nt.model;
 
 import java.time.LocalDateTime;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class ErrorDetails {
 	
 	//custom property names
