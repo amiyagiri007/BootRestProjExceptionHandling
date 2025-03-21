66cadb7f4e631d5c594ae1dd4babdf9d4f19cc44
amiyagiri007 <amiyakrushnagiri430@gmail.com>
2025-03-22 00:04:45 +0530

    1 added. by Pk

--- src/main/java/com/nt/model/ErrorDetails.java
+++ src/main/java/com/nt/model/ErrorDetails.java
@@ -35,7 +35,7 @@ public class ErrorDetails {
 		this.status = status;
 	}
 	public ErrorDetails() {
-		System.out.println("*****");
+		System.out.println("*****1");
 		System.out.println("ErrorDetails.ErrorDetails()");
 	}
 
